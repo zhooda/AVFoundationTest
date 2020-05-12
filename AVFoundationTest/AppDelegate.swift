5f8f020b9131713ade5578c89e7fdd5a1399610f
Zeeshan Hooda <zeeshanhooda@icloud.com>
2020-05-11 21:45:46 -0600

    Initial Commit

---
+++ AVFoundationTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AVFoundationTest
+//
+//  Created by Zeeshan Hooda on 5/11/20.
+//  Copyright © 2020 Deceptive Labs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AVFoundationTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AVFoundationTest
+//
+//  Created by Zeeshan Hooda on 5/11/20.
+//  Copyright © 2020 Deceptive Labs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
